e8c1f429849bea8fb1616c5ca893df60b2511c6f
BrianDYKim <18k7102dy@naver.com>
2022-10-08 00:29:59 +0900

    [ADD] hello1 메소드 추가

---
+++ src/main/kotlin/personal/brian/jenkinsExample/HelloController.kt
@@ -0,0 +1,17 @@
+package personal.brian.jenkinsExample
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * @author Brian
+ * @since 2022/10/07
+ */
+@RestController
+@RequestMapping("/api/v1")
+class HelloController {
+
+    @GetMapping("/hello1")
+    fun hello1(): String = "first hello!!"
+}
